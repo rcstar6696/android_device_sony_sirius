bec466e00328b91f47e6f8655cc24d6f885aafab
Max Weffers <rcstar6696@gmail.com>
2017-05-04 17:41:25 +0200

    Carbonify Xperia Z2
    Remove Twrp includes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_sirius.mk
+    $(LOCAL_DIR)/full_sirius.mk \
+    $(LOCAL_DIR)/carbon_sirius.mk
--- carbon_sirius.mk
+++ carbon_sirius.mk
@@ -1,5 +1,8 @@
-# Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+## Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
 $(call inherit-product, device/sony/sirius/full_sirius.mk)
 
@@ -7,5 +10,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
 
-PRODUCT_NAME := lineage_sirius
+PRODUCT_NAME := carbon_sirius
 PRODUCT_DEVICE := sirius
