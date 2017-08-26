ed4ddcbcb70de846932c0b84c9ebf65480fd6984
Max Weffers <rcstar6696@gmail.com>
2017-08-26 18:30:56 +0200

    Revert "Carbonify Xperia Z2"
    This reverts commit bec466e00328b91f47e6f8655cc24d6f885aafab.

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,5 @@
-## Inherit Carbon GSM telephony parts
-$(call inherit-product, vendor/carbon/config/gsm.mk)
-
-# Inherit Carbon product configuration
-$(call inherit-product, vendor/carbon/config/common.mk)
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 $(call inherit-product, device/sony/sirius/device.mk)
 
@@ -10,5 +7,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=D6503
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=Sony/D6503/D6503:6.0.1/23.5.A.1.291/2769308465:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="D6503-user 6.0.1 23.5.A.1.291 2769308465 release-keys"
 
-PRODUCT_NAME := carbon_sirius
+PRODUCT_NAME := lineage_sirius
 PRODUCT_DEVICE := sirius
